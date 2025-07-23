31f5bb12034b35b58ea86e42398193f88801170f
Qadir Adesoye <abdqadirola@gmail.com>
2025-07-23 14:55:56 +0100

    working on eco point

---
+++ SmartContract/contracts/tokens/EcoPoints.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EcoPoints is ERC20, Ownable {
+    constructor() ERC20("EcoPoints", "ECO") Ownable(msg.sender) {}
+
+    error OnlyRecycleHub();
+
+    function mint(address to, uint256 amount) external {
+        if (msg.sender != owner()) revert OnlyRecycleHub();
+        _mint(to, amount);
+    }
+}
