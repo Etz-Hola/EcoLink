cc1d305a893d8c70272d495f5e6c6c7d4d88cc5d
Qadir Adesoye <abdqadirola@gmail.com>
2025-08-08 21:46:54 +0100

    delete

--- SmartContract/contracts/Interfaces/IEcoPoints.sol
+++ SmartContract/contracts/Interfaces/IEcoPoints.sol
@@ -6,8 +6,3 @@ interface IEcoPoints {
     function balanceOf(address account) external view returns (uint256);
     function transferOwnership(address newOwner) external;
 }
-// interface IEcoPoints {
-//     function mint(address to, uint256 amount) external;
-//     function balanceOf(address account) external view returns (uint256);
-//     function transferOwnership(address newOwner) external;
-// }
