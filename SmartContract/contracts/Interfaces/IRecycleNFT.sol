2b4b4e6acae56427e67d1a3e0480fbfab54599d0
Qadir Adesoye <abdqadirola@gmail.com>
2025-10-06 21:51:16 +0100

    update

--- SmartContract/contracts/Interfaces/IRecycleNFT.sol
+++ SmartContract/contracts/Interfaces/IRecycleNFT.sol
@@ -5,5 +5,5 @@ interface IRecycleNFT {
     function mint(address to, uint256 materialId) external returns (uint256);
     function ownerOf(uint256 tokenId) external view returns (address);
     function transferOwnership(address newOwner) external;
-    // function setBaseURI(strin )
+    //  update function setBaseURI(str )
 }
